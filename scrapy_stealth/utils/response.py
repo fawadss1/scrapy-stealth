081eb86977924a3f67e11c99f82aa5c87fbc3a12
Fawad Ali <fawadstar6@gmail.com>
2026-05-01 18:13:27 +0500

    Enhance `StealthResponse`: extend functionality with `_meta` support and improved docstring

--- scrapy_stealth/utils/response.py
+++ scrapy_stealth/utils/response.py
@@ -6,7 +6,14 @@ from ..config import config
 
 
 class StealthResponse(HtmlResponse):
-    """HtmlResponse produced by stealth engines."""
+    """
+    Class for handling HTTP responses with extended functionality and customization.
+
+    The StealthResponse class provides a way to handle HTTP responses while enabling
+    modifications to request metadata and handling headers in a dictionary format. It
+    inherits from the HtmlResponse class and is designed for specific use cases where
+    custom response processing is required.
+    """
 
     def __init__(
             self,
@@ -15,7 +22,14 @@ class StealthResponse(HtmlResponse):
             headers,
             body: bytes,
             encoding: str | None = None,
+            _meta: dict | None = None,
     ) -> None:
+
+        if _meta:
+            updated_meta = request.meta.copy()
+            updated_meta.update(_meta)
+            request = request.replace(meta=updated_meta)
+
         headers_dict = self._to_dict(headers)
         super().__init__(
             url=request.url,
